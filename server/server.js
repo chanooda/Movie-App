15eb3a0bc4339d5c1a6ef949840bec84f643427a
chanooda <hanrhfqkq@gmail.com>
2022-06-19 03:54:19 +0900

    api route error

--- server/server.js
+++ server/server.js
@@ -2,13 +2,8 @@ import express from "express";
 import apiRouter from "./routers/apiRouter";
 import cors from "cors";
 import path from "path";
-import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
-const KMDB_API_KEY = process.env.KMDB_API_KEY;
 
 // 배포용 주소와 개발용 주소 화이트 리스트에 추가
 const whitelist = [
@@ -35,26 +30,10 @@ app.use(express.static(path.join(__dirname, "../build")));
 
 // 해당 path 라우터
 // cors 설정 미들웨어 (origin 주소에서만 허용)
-app.get("/api/movie/getImage", async (req, res) => {
-  try {
-    const response = await axios.get(
-      `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&listCount=1`,
-      {
-        params: {
-          ServiceKey: KMDB_API_KEY,
-          title: req.query.title,
-          releaseDts: req.query.releaseDts,
-        },
-      }
-    );
-    res.status(200).json(response.data);
-  } catch (error) {
-    res.status(404).json(error.response.data);
-  }
-});
+app.use("/api/movie", apiRouter);
 
 // root로 접근 시 이 프론트를 띄우줌
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 });
 
